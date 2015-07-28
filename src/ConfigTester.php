900dbb6dfa327ba6e82717ec7e9cdcbfa80159ee
Graham Campbell <graham@alt-three.com>
2015-07-28 15:57:54 +0100

    Added a config tester

---
+++ src/ConfigTester.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of StyleCI.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StyleCI\Fixer;
+
+use Closure;
+
+/**
+ * This is the config tester class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class ConfigTester
+{
+    /**
+     * The analyzer resolver.
+     *
+     * @var \Closure
+     */
+    protected $analyzer;
+
+    /**
+     * The local storage path.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Create a new config tester instance.
+     *
+     * @param \Closure $analyzer
+     * @param string   $path
+     *
+     * @return void
+     */
+    public function __construct(Closure $analyzer, $path)
+    {
+        $this->analyzer = $analyzer;
+        $this->path = $path;
+    }
+
+    /**
+     * Analyze the sample and return the results.
+     *
+     * @param string      $sample
+     * @param string|null $config
+     * @param string|null $header
+     *
+     * @return \StyleCI\Fixer\Results
+     */
+    public function analyze($sample, $config = null, $header = null)
+    {
+        $path = $this->path.'/'.str_random(8);
+        $file = $path.'/Test.php';
+
+        mkdir($path);
+        file_put_contents($file, $sample);
+
+        $errors = $this->getAnalyzer()->analyze($path, false, $config, $header);
+        $fixed = file_get_contents($file);
+
+        unlink($file);
+        unlink($path);
+
+        return new Results($errors, $path, $fixed);
+    }
+
+    /**
+     * Get a new analyzer instance.
+     *
+     * @return \StyleCI\Fixer\Analyzer
+     */
+    public function getAnalyzer()
+    {
+        $resolver = $this->analyzer;
+
+        return $resolver();
+    }
+}
---
+++ src/Results.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of StyleCI.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StyleCI\Fixer;
+
+use Symfony\CS\Error\ErrorsManager;
+
+/**
+ * This is the results class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class Results
+{
+    use ErrorsTrait;
+
+    /**
+     * The error manager instance.
+     *
+     * @var \Symfony\CS\Error\ErrorsManager
+     */
+    protected $errors;
+
+    /**
+     * The location of the project.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * The fixed sample.
+     *
+     * @var string
+     */
+    protected $sample;
+
+    /**
+     * Create a new results instance.
+     *
+     * @param \Symfony\CS\Error\ErrorsManager $errors
+     * @param string                          $path
+     * @param string                          $sample
+     *
+     * @return void
+     */
+    public function __construct(ErrorsManager $errors, $path, $sample)
+    {
+        $this->errors = $errors;
+        $this->path = $path;
+        $this->sample = $sample;
+    }
+
+    /**
+     * Get the fixed sample.
+     *
+     * @return string
+     */
+    public function sample()
+    {
+        return $this->sample;
+    }
+}
