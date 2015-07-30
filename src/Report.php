b0bb1b062d0d0afbe2e934f731d538c28afe0a92
Graham Campbell <graham@alt-three.com>
2015-07-30 23:33:41 +0100

    Removed broken successful function

--- src/Report.php
+++ src/Report.php
@@ -68,14 +68,4 @@ class Report
     {
         return $this->diff;
     }
-
-    /**
-     * Was the analysis successful?
-     *
-     * @return bool
-     */
-    public function successful()
-    {
-        return $this->diff === '';
-    }
 }
